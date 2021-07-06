1a8b971ef4276f01f286eabbe9c2e8070407766a
Josenias Nascimento <siberiun@gmail.com>
2021-07-06 09:20:49 -0300

    Bugfix - Atualização da imagem de perfil

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
 import { IonicPage, LoadingController, NavController, NavParams } from 'ionic-angular';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { CameraOptions, Camera } from '@ionic-native/camera';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @IonicPage()
 @Component({
@@ -16,6 +17,7 @@ export class ProfilePage {
 
   cliente: ClienteDTO;
   picture: string;
+  profileImage;
   cameraOn: boolean = false;
 
   constructor(
@@ -24,7 +26,10 @@ export class ProfilePage {
     public storage: StorageService,
     public clienteService: ClienteService,
     public camera: Camera,
-    public loadingCtrl: LoadingController) {
+    public loadingCtrl: LoadingController,
+    public sanitizer: DomSanitizer) {
+
+    this.profileImage = 'assets/imgs/avatar-blank.png';
   }
 
   ionViewDidLoad() {
@@ -51,10 +56,27 @@ export class ProfilePage {
   }
 
   getImageIfExists() {
-    this.clienteService.getImageFromBucket(this.cliente.id).subscribe(response => {
+    this.clienteService.getImageFromBucket(this.cliente.id)
+    .subscribe(response => {
       this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+      this.blobToDataURL(response).then(dataUrl => {
+        let str : string = dataUrl as string;
+        this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
+      });
     },
-      error => { });
+    error => {
+      this.profileImage = 'assets/imgs/avatar-blank.png';
+    });
+  }
+
+  // https://gist.github.com/frumbert/3bf7a68ffa2ba59061bdcfc016add9ee
+  blobToDataURL(blob) {
+    return new Promise((fulfill, reject) => {
+        let reader = new FileReader();
+        reader.onerror = reject;
+        reader.onload = (e) => fulfill(reader.result);
+        reader.readAsDataURL(blob);
+    })
   }
 
   getCameraPicture() {
@@ -103,8 +125,8 @@ export class ProfilePage {
     this.clienteService.uploadPicture(this.picture)
       .subscribe(response => {
         this.picture = null;
+        this.getImageIfExists();
         loader.dismiss();
-        this.loadData();
       }, error => {
         loader.dismiss();
       });
